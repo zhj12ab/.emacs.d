8afc096a5c48f69d3b3d029b421cbba9b6bbff4d
ZengHuaJian <zhj12ab@163.com>
2020-06-03 18:27:23 +0800

    change theme to dracula

--- .custom.el
+++ .custom.el
@@ -67,8 +67,10 @@
 (add-hook 'emacs-lisp-mode-hook 'show-paren-mode)
 
 ;; 主题配置
+(require-package 'dracula-theme)
 ;; (load-theme 'solarized-dark t)
-(load-theme 'doom-molokai t)
+;; (load-theme 'doom-molokai t)
+(load-theme 'dracula t)
 
 
 ;; 新快捷键设置
