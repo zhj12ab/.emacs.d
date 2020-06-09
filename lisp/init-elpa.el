b77917f68c5dbf6e372aec9a3fa5c3c8074e02cc
ZengHuaJian <zhj12ab@163.com>
2020-06-09 14:04:44 +0800

    upgrade from chenbin

--- lisp/init-elpa.el
+++ lisp/init-elpa.el
@@ -393,6 +393,7 @@ You still need modify `package-archives' in \"init-elpa.el\" to PERMANENTLY use 
      espresso-theme
      exotica-theme
      eziam-theme
+     fantom-theme
      farmhouse-theme
      flatland-theme
      flatui-theme
@@ -461,6 +462,7 @@ You still need modify `package-archives' in \"init-elpa.el\" to PERMANENTLY use 
      twilight-theme
      ujelly-theme
      underwater-theme
+     vscode-dark-plus-theme
      white-sand-theme
      zen-and-art-theme
      zenburn-theme
